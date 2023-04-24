1c2038bc5b22b96c34c57345568f901c96889b4a
penumalludurgamanikantareddy <ashokreddypenumallu@gmail.com>
2023-04-24 22:03:01 +0530

    app js changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Services from './components/Services';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-    <Navbar />
+   
+<Navbar />
+
+<Header/>
+<Services/>
+<Footer/>
     </div>
   );
 }
